c1ba093d971c456459327a8622f89e4fabc349d3
Hariom Nagar <hari.nagar45@gmail.com>
2026-01-23 14:14:45 +0530

    Eshop microservice full code commit

---
+++ src/WebApps/Shopping.Web/Services/IOrderingService.cs
@@ -0,0 +1,16 @@
+﻿namespace Shopping.Web.Services
+{
+    public interface IOrderingService
+    {
+        // ordering pram needs to be same as in ordering.API for refit mapping
+        [Get("/ordering-service/orders?pageIndex={pageIndex}&pageSize={pageSize}\"")]
+        Task<GetOrdersResponse> GetOrders(int? pageIndex = 1, int? pageSize = 10);
+
+        [Get("/ordering-service/orders/{orderName}")]
+        Task<GetOrdersByNameResponse> GetOrdersByName(string orderName);
+
+        [Get("/ordering-service/orders/customer/{customerId}")]
+        Task<GetOrdersByCustomerResponse> GetOrdersByCustomer(Guid customerId);
+
+    }
+}
